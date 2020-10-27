1846ab3ad068940964274e79036981748bbee953
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2020-10-27 10:55:40 -0400

    comment out script

--- integrations-intro/integrations-intro-1/scripts/init.sh
+++ integrations-intro/integrations-intro-1/scripts/init.sh
@@ -1,14 +1,14 @@
 # Update apt
-echo "Updating apt..."
-apt update
+#echo "Updating apt..."
+#apt update
 
 # Install virtualenv package
-echo "Installing python development packages..."
-apt install -y python3.8-venv python3.8-dev
+#echo "Installing python development packages..."
+#apt install -y python3.8-venv python3.8-dev
 
 # Ensure python3 is default
-echo "Setting python3 as the default..."
-update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+#echo "Setting python3 as the default..."
+#update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 
 # Ready to go!
 echo "You're good to go!"
