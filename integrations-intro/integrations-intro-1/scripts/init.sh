04ec82e432e775b6c02e45c6d4cf469d457bf4b3
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2020-10-27 10:35:12 -0400

    move init to foreground

--- integrations-intro/integrations-intro-1/scripts/init.sh
+++ integrations-intro/integrations-intro-1/scripts/init.sh
@@ -1,16 +1,16 @@
 # Update apt
-echo "Updating apt..." | wall -n
+echo "Updating apt..."
 apt update
 
 # Install virtualenv package
-echo "Installing python development packages..." | wall -n
+echo "Installing python development packages..."
 apt install -y python3.8-venv python3.8-dev
 
 # Ensure python3 is default
-echo "Setting python3 as the default..." | wall -n
+echo "Setting python3 as the default..."
 update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 
 # Ready to go!
-echo "You're good to go!" | wall -n
+echo "You're good to go!"
 
 exit 0
