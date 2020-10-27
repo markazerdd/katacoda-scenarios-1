72364be9a90fc8180d5b88c0235cd24325df2a06
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2020-10-27 11:01:04 -0400

    make python3 defauly

--- integrations-intro/integrations-intro-1/scripts/init.sh
+++ integrations-intro/integrations-intro-1/scripts/init.sh
@@ -7,8 +7,8 @@
 #apt install -y python3.8-venv python3.8-dev
 
 # Ensure python3 is default
-#echo "Setting python3 as the default..."
-#update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+echo "Setting python3 as the default..."
+update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 
 # Ready to go!
 echo "You're good to go!"
