89d96c887c0e46d2f4ed7bd09f0be5eab8efc538
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2020-11-10 15:15:24 -0500

    add timer

--- monitoringk8s/k8s1-install/foreground.sh
+++ monitoringk8s/k8s1-install/foreground.sh
@@ -5,10 +5,15 @@ while [ ! -f "/usr/local/bin/prepenvironment" ]; do
 done
 sleep 0.3
 
+kubeloopstart=`date +%s`
 until kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 do
-  echo "kubectl isn't ready yet. If this doesn't resolve after 45 seconds, contact support."
-  sleep 1.5
+  kubeloopend=`date +%s`
+  kubeloopruntime=$((kubeloopend-kubeloopstart))
+  echo "kubectl isn't ready yet."
+  echo "It has been $kubeloopruntime seconds"
+  echo "If this doesn't resolve after 60 seconds, contact support."
+  sleep 2
 done
 
 
