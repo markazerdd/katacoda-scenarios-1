1063a8a26e6d16791125303e9e18781318cb7a39
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2020-11-10 15:03:01 -0500

    try loop

--- monitoringk8s/k8s1-install/foreground.sh
+++ monitoringk8s/k8s1-install/foreground.sh
@@ -5,7 +5,12 @@ while [ ! -f "/usr/local/bin/prepenvironment" ]; do
 done
 sleep 0.3
 
-kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
+until kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
+do
+  echo "kubectl isn't ready yet. If this doesn't resolve, contact support."
+  sleep 0.5
+done
+
 
 
 prepenvironment
