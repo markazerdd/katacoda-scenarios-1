070bf5159d5145d0e74039d07f3d8b0453c876a0
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2021-01-14 14:51:17 -0500

    add period

--- integrations-intro-od/integrations-intro-1/scripts/init.sh
+++ integrations-intro-od/integrations-intro-1/scripts/init.sh
@@ -7,7 +7,7 @@
 #apt install -y python3.8-venv python3.8-dev
 
 # Ensure python3 is default
-echo "Setting python3 as the default..."
+echo "Setting python3 as the default....."
 update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 apt-get -y update
 apt-get -y install python3.8-venv
