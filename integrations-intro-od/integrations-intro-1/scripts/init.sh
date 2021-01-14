f1cb2e27915f26dbfcd7719143451136afa092ba
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2021-01-14 14:47:00 -0500

    more testing

--- integrations-intro-od/integrations-intro-1/scripts/init.sh
+++ integrations-intro-od/integrations-intro-1/scripts/init.sh
@@ -12,6 +12,9 @@ update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 apt-get -y update
 apt-get -y install python3.8-venv
 # Ready to go!
+echo "does this show up"
+echo "what about this' "
+echo "and now!"
 echo "You're good to go! - 1"
 echo "You're good to go! - 2"
 # exit 0
