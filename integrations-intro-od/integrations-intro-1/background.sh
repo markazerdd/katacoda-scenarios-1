74798d1cf9026d614694b4c128001e097e71e341
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2021-01-19 23:01:30 -0500

    update

--- integrations-intro-od/integrations-intro-1/background.sh
+++ integrations-intro-od/integrations-intro-1/background.sh
@@ -1,3 +1,4 @@
+curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 apt-get -y update
 apt-get -y install python3.8-venv </dev/null
