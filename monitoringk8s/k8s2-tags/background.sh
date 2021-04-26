fcf30445cb9cf276df0762ab79d26ca93df8ada1
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2021-04-26 11:42:36 -0400

    monitor k8s 2

--- monitoringk8s/k8s2-tags/background.sh
+++ monitoringk8s/k8s2-tags/background.sh
@@ -4,6 +4,10 @@ touch status.txt
 echo "">/root/status.txt
 if [ ! -f "/root/provisioned" ]; then
   apt install datamash
+  cd k8s-yaml-files
+  sudo sed -i 's/extensions\/v1beta1/apps\/v1/g' datadog-agent.yaml
+  sudo sed -i 's/6.10.2/6.27.0/' datadog-agent.yaml #agent version
+  sudo sed -i '/template:/i \ \ selector:\n\ \ \ \ matchLabels:\n\ \ \ \ \ \ app:\ datadog-agent' datadog-agent.yaml
 fi
 
 echo "complete">>/root/status.txt
