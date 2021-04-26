75b4933f213b5256f077e91c7d72a83616db8f76
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2021-04-26 13:55:39 -0400

    add loop

--- monitoringk8s/k8s2-tags/foreground.sh
+++ monitoringk8s/k8s2-tags/foreground.sh
@@ -9,7 +9,7 @@ until kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 do
   kubeloopend=`date +%s`
   kubeloopruntime=$((kubeloopend-kubeloopstart))
-  echo "kubectl isn't ready yet."
+  echo "kubectl isn't ready yet (create secret)."
   echo "It has been $kubeloopruntime seconds"
   echo "If this doesn't resolve after 60 seconds, contact support."
   sleep 2
@@ -19,13 +19,21 @@ until kubectl apply -f k8s-yaml-files/lotsofpods.yaml
 do
   kubeloopend=`date +%s`
   kubeloopruntime=$((kubeloopend-kubeloopstart))
-  echo "kubectl isn't ready yet."
+  echo "kubectl isn't ready yet (lotsofpods)."
   echo "It has been $kubeloopruntime seconds"
   echo "If this doesn't resolve after 60 seconds, contact support."
   sleep 2
 done
 
-kubectl apply -f k8s-yaml-files/datadog.yaml
+until kubectl apply -f k8s-yaml-files/datadog.yaml
+do
+  kubeloopend=`date +%s`
+  kubeloopruntime=$((kubeloopend-kubeloopstart))
+  echo "kubectl isn't ready yet (datadog)."
+  echo "It has been $kubeloopruntime seconds"
+  echo "If this doesn't resolve after 60 seconds, contact support."
+  sleep 2
+done
 clear
 
 prepenvironment
