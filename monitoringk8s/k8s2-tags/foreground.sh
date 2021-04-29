086094cdb4605f739d75cebe34a1c2160b1e176d
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2021-04-29 11:36:45 -0400

    change to waitfork8s

--- monitoringk8s/k8s2-tags/foreground.sh
+++ monitoringk8s/k8s2-tags/foreground.sh
@@ -4,16 +4,7 @@ while [ ! -f "/usr/local/bin/prepenvironment" ]; do
 done
 sleep 0.3
 
-kubeloopstart=`date +%s`
-until kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
-do
-  kubeloopend=`date +%s`
-  kubeloopruntime=$((kubeloopend-kubeloopstart))
-  echo "kubectl isn't ready yet (create secret)."
-  echo "It has been $kubeloopruntime seconds"
-  echo "If this doesn't resolve after 60 seconds, contact support."
-  sleep 2
-done
+waitfork8s
 
 until kubectl apply -f k8s-yaml-files/lotsofpods.yaml
 do
