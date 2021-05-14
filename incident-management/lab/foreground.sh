8d1bd314251fea4ba375cd8b0e7e5e14883361de
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2021-05-14 14:05:00 -0400

    update script

--- incident-management/lab/foreground.sh
+++ incident-management/lab/foreground.sh
@@ -2,8 +2,38 @@
 
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
-cd /ecommworkshop/docker-compose-files
+cd /ecommworkshop/deploy/docker-compose
 docker-compose -f docker-compose-broken-instrumented.yml up -d
+clear
+
+curl -X POST "https://api.datadoghq.com/api/v1/monitor" \
+-H "Content-Type: application/json" \
+-H "DD-API-KEY: ${DD_API_KEY}" \
+-H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
+-d @- << EOF
+{
+  "name" : "Monitor for Incident Management Course",
+  "type" : "metric alert",
+  "message" : "`store-frontend` average latency is too high.",
+  "query" : "avg(last_1m):avg:trace.rack.request{env:ruby-shop,resource_name:spree::homecontroller_index,service:store-frontend} > 1",
+  "options": {
+    "notify_audit": false,
+    "locked": false,
+    "timeout_h": 0,
+    "new_host_delay": 300,
+    "require_full_window": false,
+    "notify_no_data": false,
+    "renotify_interval": "0",
+    "escalation_message": "",
+    "no_data_timeframe": null,
+    "include_tags": true,
+    "thresholds": {
+      "critical": 1
+    }
+  },
+  "tags" : ["service:store-frontend", "env:ruby-shop", "resource_name:spree::homecontroller_index"]
+}
+EOF
 clear
 
 envready
