bd3cd4e1b3871b1c404ef843aa3993c8066452a0
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2021-08-04 13:23:53 -0400

    add dummy accounts:

--- incident-management/lab/foreground.sh
+++ incident-management/lab/foreground.sh
@@ -49,6 +49,99 @@ curl -X POST "https://api.datadoghq.com/api/v1/monitor" \
 EOF
 
 
+ONCALL_EMAIL="$(echo $LABUSER | sed 's/@/+oncall_engineer@/')"
+AD_SERVICE_EMAIL="$(echo $LABUSER | sed 's/@/+ad_service_engineer@/')"
+
+API_RESPONSE=$(curl -X POST "https://api.datadoghq.com/api/v2/users" \
+-H "Content-Type: application/json" \
+-H "DD-API-KEY: ${DD_API_KEY}" \
+-H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
+-d @- << EOF
+{
+  "data": {
+    "attributes": {
+      "email": "${ONCALL_EMAIL}",
+      "name": "On-call Engineer"
+    },
+    "type": "users"
+  }
+}
+EOF
+)
+
+ONCALL_EXISTS=$(echo $API_RESPONSE | jq 'has("errors")')
+
+if ! $ONCALL_EXISTS
+then
+    CONFIRM_ID=$(echo $API_RESPONSE | jq '.data.id' --raw-output)
+    curl -X POST "https://api.datadoghq.com/api/v2/user_invitations" \
+    -H "Content-Type: application/json" \
+    -H "DD-API-KEY: ${DD_API_KEY}" \
+    -H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
+    -d @- << EOF
+    {
+    "data": [
+        {
+        "relationships": {
+            "user": {
+            "data": {
+                "id": "${CONFIRM_ID}",
+                "type": "users"
+            }
+            }
+        },
+        "type": "user_invitations"
+        }
+    ]
+    }
+EOF
+fi
+
+API_RESPONSE=$(curl -X POST "https://api.datadoghq.com/api/v2/users" \
+-H "Content-Type: application/json" \
+-H "DD-API-KEY: ${DD_API_KEY}" \
+-H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
+-d @- << EOF
+{
+  "data": {
+    "attributes": {
+      "email": "${AD_SERVICE_EMAIL}",
+      "name": "Ad Service Engineer"
+    },
+    "type": "users"
+  }
+}
+EOF
+)
+
+AD_SERVICE_EXISTS=$(echo $API_RESPONSE | jq 'has("errors")')
+
+if ! $AD_SERVICE_EXISTS
+then
+    CONFIRM_ID=$(echo $API_RESPONSE | jq '.data.id' --raw-output)
+    curl -X POST "https://api.datadoghq.com/api/v2/user_invitations" \
+    -H "Content-Type: application/json" \
+    -H "DD-API-KEY: ${DD_API_KEY}" \
+    -H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
+    -d @- << EOF
+    {
+    "data": [
+        {
+        "relationships": {
+            "user": {
+            "data": {
+                "id": "${CONFIRM_ID}",
+                "type": "users"
+            }
+            }
+        },
+        "type": "user_invitations"
+        }
+    ]
+    }
+EOF
+fi
+
 envready
 
 statusupdate complete
--- incident-management/lab/step4.md
+++ incident-management/lab/step4.md
@@ -1,6 +1,6 @@
 Now that you have a general idea of the issue, its time to determine the root cause and remediate it. In the `Remediation` tab of the incident, you can add related documents for addressing this type of issue. You can also create and assign tasks here related to the incident.
 
-Start by adding one assigned to you for "Look at app traces @[you]" (when you type the `@` symbol, you should get a dropdown to select your Datadog training account). Note that you can adjust the due date or assignees after it is created.
+Start by adding one assigned to you for "Look at app traces @[On-call Engineer]" (when you type the `@` symbol, you should get a dropdown to the account of the On-call Engineer). Note that you can adjust the due date or assignees after it is created.
 
 In another tab, take another look at the monitor that alerted you to the issue. Click on the Evaluation Graph and then **Related Traces**:
 ![Related Traces](assets/related_traces.png)
@@ -13,6 +13,6 @@ Note that they will be green since the requests are completing successfully, jus
 Click the **Open Full Page** link at the top to open this trace as a full page. Now you can add this trace to the incident so your coworkers can see what you're referencing. Open the Datadog clipboard (**Cmd/Ctrl + Shift + K**), click **Add current page** and then **Add selected item to...** to add this trace to your incident.
 ![Add Trace](assets/trace_cb.png)
 
-You can now mark the trace investigation task complete in your incident and add an additional one to "Investigate ad service code @[you]". Back in the incident overview, you can also add the "advertisements-service" as a service related to the incident.
+You can now mark the trace investigation task complete in your incident and add an additional one to "Investigate ad service code @[Ad Service Engineer]" (Assign this to the ad service engineer in your organization). Back in the incident overview, you can also add the "advertisements-service" as a service related to the incident.
 
 Take a look at the `ads-service/ads.py`{{open}} file and see if you can find the source of this latency. 
